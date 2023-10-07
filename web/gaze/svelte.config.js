e6cc7894c63d28f708c0d2afad963c3eae80e4f3
Ben Dow <benjamindow1@gmail.com>
2023-10-06 22:01:00 -0400

    tailwind

--- web/gaze/svelte.config.js
+++ web/gaze/svelte.config.js
@@ -1,4 +1,5 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -17,7 +18,8 @@ const config = {
 				strict: true
 			}
 		)
-	}
+	},
+	preprocess: vitePreprocess()
 };
 
 export default config;
